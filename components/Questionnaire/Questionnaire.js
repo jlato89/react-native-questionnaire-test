9df9c65c5f9342191e01e4da4632fe4245f36c76
jlatour88 <jlatour88@gmail.com>
2020-03-11 00:45:36 -0500

    added switch statement and moved box container styling to components

--- components/Questionnaire/Questionnaire.js
+++ components/Questionnaire/Questionnaire.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, TextInput } from 'react-native';
 import UserDetails from './components/UserDetails';
+import QuestionList from './components/QuestionList';
 
 export class Questionnaire extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      step: 1,
+      step: 2,
       firstName: '',
       lastName: '',
       dob: '',
@@ -46,10 +47,36 @@ export class Questionnaire extends Component {
     this.setState({ [name]: value });
   }
 
+
   render() {
     const { step } = this.state;
     const { firstName, lastName, dob, relapses, substancesUsed, rememberUsing, datesUsedWeed, datesUsedCocaine, datesUsedTobacco, datesUsedAlcohol, profileImg, weedUsedWith, cocaineUsedWith, tobaccoUsedWith, alcoholUsedWith, knownDaysSober, meetingsAttended, recoveryFeeling } = this.state;
     const values = { firstName, lastName, dob, relapses, substancesUsed, rememberUsing, datesUsedWeed, datesUsedCocaine, datesUsedTobacco, datesUsedAlcohol, profileImg, weedUsedWith, cocaineUsedWith, tobaccoUsedWith, alcoholUsedWith, knownDaysSober, meetingsAttended, recoveryFeeling }
+
+    let CurrentQuestions = '';
+    switch (step) {
+      case 1:
+        CurrentQuestions = (
+          <UserDetails
+            values={values}
+            fieldChangeHandler={this.handleChange}
+            nextStep={this.nextQuestion}
+          />
+        );
+        break;
+      case 2:
+        CurrentQuestions = (
+          <QuestionList
+            values={values}
+            fieldChangeHandler={this.handleChange}
+            nextStep={this.nextQuestion}
+            prevStep={this.prevQuestion}
+          />
+        );
+        break;
+      default:
+        break;
+    }
 
     return (
       <View style={styles.container}>
@@ -58,12 +85,8 @@ export class Questionnaire extends Component {
           <Text style={styles.headerText}>2:30pm</Text>
           <Text style={styles.headerText}>It has been XX days since your last visit</Text>
         </View>
-        <View style={styles.box}>
-          <UserDetails
-            values={values}
-            fieldChangeHandler={this.handleChange}
-            nextStep={this.nextQuestion}
-          />
+        <View style={styles.questionBox}>
+          {CurrentQuestions}
         </View>
       </View>
     )
@@ -72,8 +95,6 @@ export class Questionnaire extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'space-between',
     padding: 40,
   },
   header: {
@@ -83,20 +104,8 @@ const styles = StyleSheet.create({
     marginBottom: '5%'
   },
   headerText: {
-    fontSize: 30,
-
-  },
-  box: {
-    flex: 1,
-    alignItems: 'center',
-    alignSelf: 'center',
-    // justifyContent: 'center',
-    // height: '85%',
-    width: '98%',
-    borderColor: 'black',
-    borderStyle: 'solid',
-    borderWidth: 1,
-    paddingVertical: '2%'
+    fontSize: 25,
+    color: 'grey'
   }
 })
 
