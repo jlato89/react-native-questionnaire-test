b6cf69ba6daa0d867abe411c80fac6fba563271e
jlatour88 <jlatour88@gmail.com>
2020-03-11 20:01:07 -0500

    Layout for Questionnaire

---
+++ components/Questionnaire/components/Layout/MainLayout.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { View, Text, StyleSheet, TouchableWithoutFeedback, Keyboard } from 'react-native'
+
+const MainLayout = (props) => {
+  return (
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>Tuesday, March 2</Text>
+          <Text style={styles.headerText}>2:30pm</Text>
+          {props.lastVisit &&
+            <Text style={styles.headerText}>It has been {props.lastVisit} days since your last visit</Text>}
+        </View>
+        <View style={styles.mainContent}>
+          {props.children}
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: '3%',
+    paddingBottom: '10%', //? Temp solution for tablet nav bar
+  },
+  header: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '80%',
+  },
+  headerText: {
+    fontSize: 25,
+  },
+  mainContent: {
+    flex: 4,
+  }
+});
+
+
+export default MainLayout
